de04c748d96cd53e11c9ca7b530d5f87adbb34de
caliofra <francescocalio@hotmail.it>
2017-05-22 12:53:37 +0200

    file modificato

--- src/CiaoM.java
+++ src/CiaoM.java
@@ -3,6 +3,8 @@ public class CiaoM {
 	public static void main(String[]args){
 		System.out.print("ciao mondo");
 		System.out.print("Ciao mondo, vi saluta Riccardo");
+		System.out.print("Ciao mondo, vi saluta Francesco");
+		
 		
 	}
 	
